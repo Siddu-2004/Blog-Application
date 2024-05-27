632d18cd50e80ef1481d5ada1ad453a3ed5fe6bc
Siddu-2004 <162559669+Siddu-2004@users.noreply.github.com>
2024-05-27 09:49:15 +0530

    Update admin-api.js

--- backend/APIs/admin-api.js
+++ backend/APIs/admin-api.js
@@ -7,6 +7,40 @@ adminApp.get('/test-admin',(req,res)=>{
     res.send({message:"This from admin api"})
 })
 
+adminApp.post(
+  "/login",
+  expressAsyncHandler(async (req, res) => {
+    //get cred obj from client
+    const adminCred = req.body;
+    //check for username
+    const dbadmin = await usercollection.findOne({
+      username: adminCred.username,
+    });
+    if (dbadmin === null) {
+      res.send({ message: "Invalid username" });
+    } else {
+      //check for password
+      const status = await bcryptjs.compare(adminCred.password, dbadmin.password);
+      if (status === false) {
+        res.send({ message: "Invalid password" });
+      } else {
+        //create jwt token and encode it
+        const signedToken = jwt.sign(
+          { username: dbadmin.username },
+          process.env.SECRET_KEY,
+          { expiresIn: '1d' }
+        );
+        //send res
+        res.send({
+          message: "login success",
+          token: signedToken,
+          user: dbadmin,
+        });
+      }
+    }
+  })
+);
+
 
 //export userApp
 module.exports=adminApp;
